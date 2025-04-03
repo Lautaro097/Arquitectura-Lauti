491acbd4cc7fcff82704a1ea06f61e0a781cbb4d
Lautaro097 <lautarozavalla@uca.edu.ar>
2025-04-03 19:08:44 -0300

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,17 @@
+
+frutas = ["manzana"," Pera","uva","mandarina"]
+for fruta in frutas:
+    print(fruta)
+    
+    
+#while #repite un bloque mientras una condicion se verdadera
+    
+contador=0
+
+while True:
+    print(contador)
+    contador +=1
+    if contador == 5:
+        break
+    
+    
